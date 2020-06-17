c33c34faa4b2c8c2d51292f0808ccb7e34b55d0f
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2020-06-17 10:24:02 +0530

    React Span for SEO

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {hydrate, render } from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <React.Fragment>
-    <App />
-  </React.Fragment>,
-  document.getElementById('root')
-);
+
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
